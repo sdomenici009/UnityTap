59c1ce20aa0e766463e2388c7c55d41c1069fc9a
Stephen Domenici <sdomenici009@gmail.com>
2014-03-24 15:12:28 -0700

    Added enemies and prefabs.

--- Tap/Assets/Enemy.cs
+++ Tap/Assets/Enemy.cs
@@ -1,27 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
 
-public class Ball : MonoBehaviour {
+public class Enemy : MonoBehaviour {
 	
 	public GameManager gm;
 	Vector3 target;
 	
 	void Start () {
-		transform.localScale = new Vector3(.3f, .3f, .3f);
-		transform.rotation = new Quaternion(0, 90, 90, 0);
-		renderer.material.color  = Color.black;	
-		target = new Vector3(25, 25, 0);
+		target = new Vector3(Random.Range (-45, 45), Random.Range (-45, 45), 0);
 	}
 	
 	void Update () {
-		if(Input.GetMouseButtonDown(0))
-		{	
-			Vector3 temp = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0);
-			
-			target = (temp - gameObject.transform.position).normalized * 20;
-		}
-		
 		if(collider.bounds.Intersects(gm.wall1.collider.bounds) || 
 		   collider.bounds.Intersects(gm.wall2.collider.bounds))
 		{
---
+++ Tap/Assets/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	GameObject ball;
+	GameObject screenColliders;
+	public GameObject wall1, wall2, wall3, wall4;
+	
+	void Start () {
+		Random.seed = System.Environment.TickCount;
+		wall1.renderer.material.color = Color.black;
+		wall2.renderer.material.color = Color.black;
+		wall3.renderer.material.color = Color.black;
+		wall4.renderer.material.color = Color.black;
+		ball = (GameObject)Instantiate(Resources.Load("Ball"));
+		ball.GetComponent<Ball>().gm = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Random.Range(0, 100) == 0)
+		{
+			GameObject enemy = (GameObject)Instantiate(Resources.Load ("Enemy")); 
+			enemy.GetComponent<Enemy>().gm = this;
+		}
+	}
+}
